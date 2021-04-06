32f933701c4edeb5d98897a03e232dbad0a7e5f4
val898 <val898@users.noreply.github.com>
2021-04-06 02:04:54 -0400

    Update Accelerometer.js

--- 3Acc_web/Accelerometer.js
+++ 3Acc_web/Accelerometer.js
@@ -4,16 +4,15 @@ let myCharacteristic;
 let myValue = 0;
 let myBLE;
 
+// //  an instance of the serialport library:
+// let serial;
+
 function setup() {
   colorMode(RGB, 100);
   // Create a p5ble class
   myBLE = new p5ble();
 
-  canvas = createCanvas(200, 200);
-  canvas.stroke(128, 128, 128);
-  canvas.position(60, 80);
-  textSize(20);
-  textAlign(80, 120);
+  canvas = createCanvas(200, 200, WEBGL);
 
   // Create a 'Connect' button
   const connectButton = createButton('Connect')
@@ -42,22 +41,8 @@ function gotCharacteristics(error, characteristics) {
   console.log('characteristics: ', characteristics);
   myCharacteristic = characteristics[0];
   // Read the value of the first characteristic
-  // myBLE.read(myCharacteristic, gotValue);
   myBLE.startNotifications(myCharacteristic, handleNotifications);
 }
-
-// // A function that will be called once got values
-// function gotValue(error, value) {
-//   if (error) console.log('error: ', error);
-//   console.log('value: ', value);
-//   myValue = value;
-//   // After getting a value, call p5ble.read() again to get the value again
-//   // myBLE.read(myCharacteristic, gotValue);
-//   myBLE.startNotifications(myCharacteristic, gotValue);
-//   // You can also pass in the dataType
-//   // Options: 'unit8', 'uint16', 'uint32', 'int8', 'int16', 'int32', 'float32', 'float64', 'string'
-//   // myBLE.read(myCharacteristic, 'string', gotValue);
-// }
 
 // A function that will be called once got characteristics
 function handleNotifications(data) {
@@ -70,24 +55,71 @@ function stopNotifications() {
   myBLE.stopNotifications(myCharacteristic);
 }
 
-function draw() {
-  if(myValue === 1) {
-    background(0, 255, 255);
-    text("CYAN");
-  }
-  else if (myValue === 2) {
-    background(255, 255, 0);
-    text("YELLOW");
-  }
-  else if (myValue === 3) {
-    background(255, 0, 255);
-    text("PINK");
-  }
-  else if (myValue === 4) {
-    background(0, 255, 0);
-    text("GREEN");
-  }
-  else {
-    background(128, 128, 128);
+// orientation variables:
+let heading = 0.0;
+let pitch = 0.0;
+let roll = 0.0;
+
+function serialEvent() {
+  // read from port until new line:
+  let message = serial.readStringUntil('\n');
+  if (message != null) {
+    let list = split(trim(message), ',');
+    if (list.length >= 3) {
+      // conver list items to floats:
+      heading = float(list[0]);
+      pitch = float(list[1]);
+      roll = float(list[2]);
+      console.log(heading + ',' + pitch + ',' + roll);
+      // send a byte to the microcontroller to get new data:
+      serial.write('x');
+    }
   }
 }
+
+// draws the Arduino Nano:
+function drawArduino() {
+  // the base board:
+  stroke(0, 90, 90); // set outline color to darker teal
+  fill(0, 130, 130); // set fill color to lighter teal
+  box(300, 10, 120); // draw Arduino board base shape
+
+  // the CPU:
+  stroke(0);         // set outline color to black
+  fill(80);          // set fill color to dark grey
+  translate(30, -6, 0); // move to correct position
+  box(60, 0, 60);    // draw box
+
+  // the radio module:
+  stroke(80);       // set outline color to grey
+  fill(180);        // set fill color to light grey
+  translate(80, 0, 0); // move to correct position
+  box(60, 15, 60);  // draw box
+
+  // the USB connector:
+  translate(-245, 0, 0); // move to correct position
+  box(35, 15, 40);   // draw box
+
+
+  function draw() {
+    // update the drawing:
+    background(255); // set background to white
+    push();          // begin object to draw
+
+    // variables for matrix translation:
+    let c1 = cos(radians(roll));
+    let s1 = sin(radians(roll));
+    let c2 = cos(radians(pitch));
+    let s2 = sin(radians(pitch));
+    let c3 = cos(radians(heading));
+    let s3 = sin(radians(heading));
+    applyMatrix(c2 * c3, s1 * s3 + c1 * c3 * s2,
+      c3 * s1 * s2 - c1 * s3, 0, -s2, c1 * c2,
+      c2 * s1, 0, c2 * s3, c1 * s2 * s3 - c3 * s1,
+      c1 * c3 + s1 * s2 * s3, 0, 0, 0, 0, 1);
+
+      // draw arduino board:
+      drawArduino();
+      pop(); // end of object
+    }
+  }
