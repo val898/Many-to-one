11ccba440a0f408ee99e6b51957525dd908f3269
val898 <val898@users.noreply.github.com>
2021-05-04 15:17:52 -0400

    Create arduino_secrets.h

---
+++ MQTT_SimpleSender_token_01_led/arduino_secrets.h
@@ -0,0 +1,8 @@
+#define SECRET_SSID "" //input Wifi name
+#define SECRET_PASS "" //input WiFi password
+
+char SHFTR_INSTANCE[] = "" ; // your SHIFTR instance name --> this = userName in arduino docs
+char SHFTR_TOKEN[] =    "" ; // your SHIFTR secret key --> this = password in arduino docs
+    // if you leave these as PUBLIC -- view @ this: https://www.shiftr.io/try
+
+char SHIFTR_ID[] = ""; // this is your tag on the shftr graph
