f3d6c9145d6e18db05dcf3b78abc546fb1ec651e
val898 <val898@users.noreply.github.com>
2021-05-08 06:22:47 -0400

    Create arduino_secrets.h

---
+++ MQTT/AccelerometerFadeLED_MQTT/senderLEDControl_IoT33_acc/arduino_secrets.h
@@ -0,0 +1,7 @@
+#define SECRET_SSID "" //input Wifi name
+#define SECRET_PASS "" //input WiFi password
+
+char SHFTR_INSTANCE[] = "public";
+char SHFTR_TOKEN[] = "public";
+
+char SHIFTR_ID[] = "IoT33"; // this is your tag on the shftr graph
