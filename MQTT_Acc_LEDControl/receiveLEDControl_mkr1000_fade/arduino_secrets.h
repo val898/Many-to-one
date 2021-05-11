43180768c7cdc81ba4de69c7211e2094a12a50f0
val898 <val898@users.noreply.github.com>
2021-05-11 16:41:30 -0400

    Update arduino_secrets.h

--- MQTT_Acc_LEDControl/receiveLEDControl_mkr1000_fade/arduino_secrets.h
+++ MQTT_Acc_LEDControl/receiveLEDControl_mkr1000_fade/arduino_secrets.h
@@ -1,8 +1,6 @@
-#define SECRET_SSID "My home :)"
-#define SECRET_PASS "Oiseau123"
+#define SECRET_SSID "" //input WiFi name
+#define SECRET_PASS "" //input Wifi password
 
-//char SHFTR_INSTANCE[] = "val898" ; // your SHIFTR instance name --> this = userName in arduino docs
-//char SHFTR_TOKEN[] =    "JuKweI9pI9I6ShUH" ; // your SHIFTR secret key --> this = password in arduino docs
     // if you leave these as PUBLIC -- view @ this: https://www.shiftr.io/try
 
 char SHFTR_INSTANCE[] = "public";
