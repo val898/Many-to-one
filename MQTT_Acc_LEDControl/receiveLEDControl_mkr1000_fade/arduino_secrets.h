a8b95afd01d02e46df04dd5ba1a66d1c93ca7311
kdanglam <kdanglam@ryerson.ca>
2021-05-14 10:34:45 -0400

    Update arduino_secrets.h

--- MQTT_Acc_LEDControl/receiveLEDControl_mkr1000_fade/arduino_secrets.h
+++ MQTT_Acc_LEDControl/receiveLEDControl_mkr1000_fade/arduino_secrets.h
@@ -1,7 +1,5 @@
-#define SECRET_SSID "" //input WiFi name
-#define SECRET_PASS "" //input Wifi password
-
-    // if you leave these as PUBLIC -- view @ this: https://www.shiftr.io/try
+#define SECRET_SSID ""
+#define SECRET_PASS ""
 
 char SHFTR_INSTANCE[] = "public";
 char SHFTR_TOKEN[] = "public";
