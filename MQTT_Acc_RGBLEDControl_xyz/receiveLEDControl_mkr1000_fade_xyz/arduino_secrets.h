37cd089c8bd7324e640e874bd620e4ae2f8d8593
kdanglam <kdanglam@ryerson.ca>
2021-05-25 11:26:25 -0400

    Create arduino_secrets.h

---
+++ MQTT_Acc_RGBLEDControl_xyz/receiveLEDControl_mkr1000_fade_xyz/arduino_secrets.h
@@ -0,0 +1,7 @@
+#define SECRET_SSID ""
+#define SECRET_PASS ""
+
+char SHFTR_INSTANCE[] = "public";
+char SHFTR_TOKEN[] = "public";
+
+char SHIFTR_ID[] = "MKR1000"; // this is your tag on the shftr graph
