d72b51b1450a2bfc7a1c87a09d3e23fde5263736
val898 <val898@users.noreply.github.com>
2021-06-21 00:41:42 -0400

    Parsed Data for 2 IoT & 2 RGB LEDs

---
+++ MQTT_Acc_RGBLEDControl_xyz/4thTest_ParsedData/senderLEDControl_IoT33_acc_xyz_v3_id0/arduino_secrets.h
@@ -0,0 +1,9 @@
+#define SECRET_SSID "" //input WiFi name
+#define SECRET_PASS "" //input Wifi password
+
+    // if you leave these as PUBLIC -- view @ this: https://www.shiftr.io/try
+
+char SHFTR_INSTANCE[] = "public";
+char SHFTR_TOKEN[] = "public";
+
+char SHIFTR_ID[] = "******IoT33_M1******"; // this is your tag on the shftr graph
