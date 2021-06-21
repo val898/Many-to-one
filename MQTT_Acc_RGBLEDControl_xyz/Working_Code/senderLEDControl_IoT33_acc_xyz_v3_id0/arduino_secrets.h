4a366395fd33e7c442b77e36360a49d2641576d5
val898 <val898@users.noreply.github.com>
2021-06-21 00:41:53 -0400

    Delete arduino_secrets.h

--- MQTT_Acc_RGBLEDControl_xyz/Working_Code/senderLEDControl_IoT33_acc_xyz_v3_id0/arduino_secrets.h
+++
@@ -1,9 +0,0 @@
-#define SECRET_SSID "" //input WiFi name
-#define SECRET_PASS "" //input Wifi password
-
-    // if you leave these as PUBLIC -- view @ this: https://www.shiftr.io/try
-
-char SHFTR_INSTANCE[] = "public";
-char SHFTR_TOKEN[] = "public";
-
-char SHIFTR_ID[] = "******IoT33_M1******"; // this is your tag on the shftr graph
