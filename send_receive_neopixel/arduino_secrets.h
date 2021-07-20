c0bf05301b358002c58213cbfbc5851b745c8bc8
val898 <val898@users.noreply.github.com>
2021-07-20 11:57:12 -0400

    Create arduino_secrets.h

---
+++ send_receive_neopixel/arduino_secrets.h
@@ -0,0 +1,10 @@
+#define SECRET_SSID "" //input WiFi name
+#define SECRET_PASS "" //input Wifi password
+
+    // if you leave these as PUBLIC -- view @ this: https://www.shiftr.io/try
+
+char SHFTR_INSTANCE[] = "public";
+char SHFTR_TOKEN[] = "public";
+
+
+//char SHIFTR_ID[] = "********MKR1000********"; // this is your tag on the shftr graph
